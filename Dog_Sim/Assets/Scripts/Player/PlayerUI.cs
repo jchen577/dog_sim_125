14418ce24d4c6abd32de27130681dd9ef3a2023f
Nick Corfmat <ncorfmat@ucsc.edu>
2024-12-02 19:56:32 -0800

    more script fixes

--- Dog_Sim/Assets/Scripts/Player/PlayerUI.cs
+++ Dog_Sim/Assets/Scripts/Player/PlayerUI.cs
@@ -8,10 +8,6 @@ using UnityEngine;
 public class PlayerUI: MonoBehaviour {
     [SerializeField]
     private TextMeshProUGUI promptText;
-    // Start is called before the first frame update
-    void Start() {
-
-    }
 
     // Update is called once per frame
     public void UpdateText(string promptMessage) {

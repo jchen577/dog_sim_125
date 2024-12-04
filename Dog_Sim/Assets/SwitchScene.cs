25034710bddb43f341cf0fe72d2a48de1948efe8
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2024-12-03 19:52:54 -0800

    scene switching

---
+++ Dog_Sim/Assets/SwitchScene.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchToPlayScene()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+    public void SwitchToGameOver()
+    {
+        SceneManager.LoadScene("GameOverScene");
+    }
+    public void SwitchToCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+    public void SwitchToMenuScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}

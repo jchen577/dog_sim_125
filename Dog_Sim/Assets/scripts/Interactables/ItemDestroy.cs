5abc5786d1aeb2b8e81ed2d0b39aa858bbc8f2d4
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-12-03 18:33:21 -0800

    Only add points if player pushes

--- Dog_Sim/Assets/scripts/Interactables/ItemDestroy.cs
+++ Dog_Sim/Assets/scripts/Interactables/ItemDestroy.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class ItemDestroy : MonoBehaviour
 {
     public ScoreManage ScoreManager;
+    public bool hasTouchedPlayer = false;
 
     void Start(){
         GameObject manager = GameObject.Find("ScoreManager"); 
@@ -13,7 +14,19 @@ public class ItemDestroy : MonoBehaviour
     }
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.tag == "Ground"){
+        if(collision.transform.tag == "Player"){
+            hasTouchedPlayer = true;
+        }
+        else{
+            ItemDestroy currObj = collision.gameObject.GetComponent<ItemDestroy>();
+            if(currObj!=null){
+                if(currObj.hasTouchedPlayer){
+                    hasTouchedPlayer=true;
+                }
+            }
+        }
+        
+        if(collision.transform.tag == "Ground" && hasTouchedPlayer){
             ScoreManager.updateScore(100);
             Destroy(gameObject);
         }
